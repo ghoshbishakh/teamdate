13776dbdc83922da0740c56c6986090f828ba7c1
Bishakh Ghosh <ghoshbishakh@gmail.com>
2017-06-20 00:08:10 +0530

    Initial environment setup

---
+++ teamdate/settings/production.py
@@ -0,0 +1,15 @@
+from teamdate.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
---
+++ teamdate/settings/secret_example.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ji3cw$i7j1%r3cf$i)+h5znx$*)j@rthy*gz)(=1$f6ycocwt!'
